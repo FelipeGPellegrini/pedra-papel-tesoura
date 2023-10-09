09ad38b8c2aa0e5c0ad0c2bc3a92da1ed0bc56cf
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-10-09 18:15:21 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
             <button className="cursor-pointer bg-slate-400 p-2 rounded w-20 mt-10 m-auto" onClick={() => {
                     setShowRules(true)
             }}>Regras</button>
-            <div className={showRules ? "fixed inset-0 flex items-center justify-center z-50 " : "hidden"}>
+            <div className={showRules ? "fixed inset-0 flex items-center justify-center z-50" : "hidden"}>
             <div className="fixed inset-0 bg-black opacity-50"></div>
-                <div className={"bg-gray-400 w-65 h-500 overflow-y-auto text-center text-white p-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl text-lg "}>
+                <div className={"bg-gray-400 w-65 h-500 overflow-y-auto text-center text-white p-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl text-lg h-2/3"}>
                 <p className="text-left cursor-pointer" onClick={() => {
                     setShowRules(false)
                 }}>X</p>
